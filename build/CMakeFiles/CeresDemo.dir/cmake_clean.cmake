9c0afd8c9d0bb68d3201aaccd94ef423964c635b
BL <yigehenqiangderen@163.com>
2024-03-05 23:18:41 +0800

    base visualizer

---
+++ build/CMakeFiles/CeresDemo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CeresDemo.dir/ceresDemo.cpp.o"
+  "CMakeFiles/CeresDemo.dir/ceresDemo.cpp.o.d"
+  "CeresDemo"
+  "CeresDemo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CeresDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CeresDemo.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CeresDemo.
+# This may be replaced when dependencies are built.
